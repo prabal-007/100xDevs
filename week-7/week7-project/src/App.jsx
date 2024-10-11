6e988aa12110c7ce38cfd1e1906877bd72e3490b
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-10-11 23:58:48 +0530

    Week - 7, lazy loading, suspense API

--- week-7/week7-project/src/App.jsx
+++ week-7/week7-project/src/App.jsx
@@ -1,18 +1,19 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+
+const Landing = lazy(() => import("./components/Landing"))
+const Dashboard = lazy(() => import("./components/Dashboard"))
 
 function App() {
-
 
   return (
     <div>
       <BrowserRouter>
         <AppBar />
         <Routes>
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/' element={<Landing />} />
+          <Route path='/dashboard' element={<Suspense fallback={"Loading..."}> <Dashboard /> </Suspense>} />
+          <Route path='/' element={<Suspense fallback={"Loading..."}> <Landing /> </Suspense>} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -21,7 +22,7 @@ function App() {
 
 function AppBar() {
   const nevigate = useNavigate();
-  
+
   return <div style={{ background: "black", color: "white", display: "flex" }}>
     {/* Hi this is top bar! */}
     <button onClick={() => {
